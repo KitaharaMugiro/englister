6e1d87f8adecb1ae624b0b8e710eb9c6266fe5a3
上川康太 <kamikawakouta@kamikawakoutanoMacBook-Air.local>
2022-05-17 22:44:19 +0900

    最初にローディングを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:englister/components/signin/LoginButton.dart';
-import 'package:englister/models/auth/AuthService.dart';
 import 'package:englister/models/riverpod/SettingRiverpod.dart';
-import 'package:englister/models/riverpod/UserRiverpod.dart';
-import 'package:englister/pages/home.dart';
-import 'package:englister/pages/phrase.dart';
 import 'package:englister/pages/plan.dart';
-import 'package:englister/pages/record.dart';
 import 'package:englister/pages/top.dart';
 import 'package:englister/route/index.dart';
 import 'package:englister/route/setting.dart';
@@ -23,19 +15,10 @@ import 'package:englister/route/top/start.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
-
-import 'amplifyconfiguration.dart';
 import 'api/graphql/client_provider.dart';
-import 'api/rest/UserApi.dart';
-import 'components/drawer/MyDrawer.dart';
-import 'components/navigation/MyBottomNavigationBar.dart';
 import 'models/localstorage/LocalStorageHelper.dart';
-import 'models/riverpod/PhraseRiverpod.dart';
-import 'models/subscriptions/listenToPurchaseUpdated.dart';
 
 Future<void> main() async {
   await initHiveForFlutter();
@@ -47,8 +30,12 @@ class MyApp extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final started = useState<bool>(false);
+    final isLoading = useState<bool>(false);
+
     Future<void> _getStarted() async {
+      isLoading.value = true;
       final res = await LocalStorageHelper.getStarted();
+      isLoading.value = false;
       if (res == null) {
         return;
       }
@@ -69,8 +56,11 @@ class MyApp extends HookConsumerWidget {
       darkTheme:
           themeMode == ThemeMode.light ? ThemeData.light() : ThemeData.dark(),
       builder: EasyLoading.init(),
-      home:
-          started.value ? const IndexPage(title: 'Englister') : const TopPage(),
+      home: isLoading.value
+          ? const Center(child: CircularProgressIndicator())
+          : started.value
+              ? const IndexPage(title: 'Englister')
+              : const TopPage(),
       routes: {
         '/index': (BuildContext context) => const IndexPage(title: 'Englister'),
         '/settings': (BuildContext context) => SettingPage(),
